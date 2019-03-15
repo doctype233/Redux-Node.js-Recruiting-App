2ac5dffa8978760d61c53e873d5c2c7b2b38ef73
qinjiamei <qjm@simplefluids.com>
2019-03-15 16:27:48 +0800

    注册模块

--- src/container/regist/index.js
+++ src/container/regist/index.js
@@ -1,54 +1,66 @@
 import React, { Component } from 'react';
 import Logo from '@/component/logo';
-import { InputItem, WhiteSpace, WingBlank, Radio,Button } from 'antd-mobile';
+import { InputItem, WhiteSpace, WingBlank, Radio, Button, Toast } from 'antd-mobile';
+import { connect } from 'react-redux';
+import { regist } from '@/redux/user.redux';
+import './index.css';
 
 
+@connect(
+    state => state.user,
+    { regist }
+)
 class Regist extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            type:'boss',
-            user:'',
-            pwd:'',
-            repeatPwd:''
+        this.state = {
+            type: 'boss',
+            user: '',
+            pwd: '',
+            repeatPwd: ''
         }
     }
-    handleChange(key,value){
+    handleChange(key, value) {
         this.setState({
-            [key]:value
+            [key]: value
         })
     }
-    handleRegist(){
-        console.log(this.state)
+    handleRegist() {
+        this.props.regist(this.state)
     }
     render() {
-        const RadioItem=Radio.RadioItem;
+        const RadioItem = Radio.RadioItem;
         return (
             <div>
+
                 <Logo></Logo>
-                <h2 style={{textAlign:'center'}}>注册</h2>
+                <h2 style={{ textAlign: 'center' }}>注册</h2>
                 <WingBlank>
                     <WhiteSpace />
-                    <InputItem onChange={(v)=>this.handleChange('user',v)}>用户名</InputItem>
-                    <InputItem onChange={(v)=>this.handleChange('pwd',v)}>密码</InputItem>
-                    <InputItem onChange={(v)=>this.handleChange('repeatPwd',v)}>确认密码</InputItem>
-                    <WhiteSpace/>
+                    
+                    <InputItem onChange={(v) => this.handleChange('user', v)}>用户名</InputItem>
+                    {
+                        this.props.msg ? <p className='error-msg'>{this.props.msg}</p> : null
+                    }
+                    <InputItem onChange={(v) => this.handleChange('pwd', v)}>密码</InputItem>
+                    <InputItem onChange={(v) => this.handleChange('repeatPwd', v)}>确认密码</InputItem>
+                    <WhiteSpace />
                     <h4>注册身份</h4>
-                    <RadioItem 
-                        checked={this.state.type=='genius'}
-                        onChange={()=>this.handleChange('type','genius')}
+                    <RadioItem
+                        checked={this.state.type == 'genius'}
+                        onChange={() => this.handleChange('type', 'genius')}
                     >
                         牛人
                     </RadioItem>
-                    <RadioItem 
-                        checked={this.state.type=='boss'}
-                        onChange={()=>this.handleChange('type','boss')}
+                    <RadioItem
+                        checked={this.state.type == 'boss'}
+                        onChange={() => this.handleChange('type', 'boss')}
 
                     >
                         BOSS
                     </RadioItem>
-                    <WhiteSpace/>
-                    <Button type='primary' onClick={()=>this.handleRegist()}>注册</Button>
+                    <WhiteSpace />
+                    <Button type='primary' onClick={() => this.handleRegist()}>注册</Button>
                 </WingBlank>
             </div>
         );
--- src/redux/user.redux.js
+++ src/redux/user.redux.js
@@ -1,48 +1,52 @@
 import axios from 'axios';
 
 const REGIST_SUCCESS = 'REGIST_SUCCESS';
-const REGIST_ERROR= 'REGIST_ERROR';
-const initState={
-    msg:'',
-    isAuth:'',
-    user:'',
-    pwd:'',
-    type:''
+const REGIST_ERROR = 'REGIST_ERROR';
+const initState = {
+    msg: '',
+    isAuth: '',
+    user: '',
+    pwd: '',
+    type: ''
 }
 //reducer，state的最终改变来自reducer
-export function user(state=initState,action){
-    switch(action.type){
+export function user(state = initState, action) {
+    switch (action.type) {
         case REGIST_SUCCESS:
-        return {...state,...action.data}
+            return { ...state, isAuthor: true, ...action.data }
         case REGIST_ERROR:
-        return {...state,...action.msg}
+            return { ...state, isAuthor: false, msg: action.msg }
         default:
-        return state
+            return state
     }
 }
 
 //页面会用到的函数
-export function regist({user,pwd,repeatPwd,type}){
-    if(!user||!pwd||!type){
+export function regist({ user, pwd, repeatPwd, type }) {
+    if (!user || !pwd || !type) {
         return errorMsg('请输入关键信息！')
     }
-    if(pwd!==repeatPwd){
+    if (pwd !== repeatPwd) {
         return errorMsg('请再次确认密码！')
     }
-    axios.post('/user/regist',{user,pwd,type}).then(res=>{
-        if(res.status===200&&res.data.code==0){
-            registSuccess({user,pwd,type})
-        }else{
-            errorMsg(res.data.msg)
-        }
-    })
-
+    return dispatch => {
+        axios.post('/user/regist', { user, pwd, type }).then(res => {
+            if (res.status === 200 && res.data.code == 0) {
+                dispatch(registSuccess({ user, pwd, type }))
+            } else {
+                dispatch(errorMsg(res.data.msg))
+            }
+        })
+    }
 }
 
 
-function errorMsg(msg){
-    return {msg,type:REGIST_ERROR}
+function errorMsg(msg) {
+    console.log(msg)
+    return { msg, type: REGIST_ERROR }
 }
-function registSuccess(data){
-    return {data,type:REGIST_SUCCESS}
+function registSuccess(data) {
+    console.log(data)
+
+    return {  data, type: REGIST_SUCCESS }
 }
