5a250053d3c9583a57c1f39b1a039c1733928d38
qinjiamei <qjm@simplefluids.com>
2019-03-15 16:27:00 +0800

    后端建立数据库

--- server/model.js
+++ server/model.js
@@ -3,3 +3,30 @@ const mongoose = require('mongoose');
 //连接mongoose,端口号后为创建的集合名称
 const DB_URL = 'mongodb://127.0.0.1:27017/meimei'
 mongoose.connect(DB_URL);
+
+const models={
+    user:{
+        'user':{type:String,require:true},
+        'pwd':{type:String,require:true},
+        'type':{type:String,require:true},
+        'avatar':{type:String},
+        'desc':{type:String},//简介
+        'title':{type:String},//职位名称
+        //boss还有额外的两个字段
+        'company':{type:String},
+        'money':{type:String}
+    },
+    chat:{
+
+    }
+}
+
+for(let m in models){
+    mongoose.model(m,new mongoose.Schema(models[m]))
+}
+
+module.exports={
+    getModel:function(name){
+        return mongoose.model(name)
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const bodyParser=require('body-parser');
+const cookieParser = require('cookie-parser');
 const userRouter=require('./user');
 
+app.use(cookieParser());
+app.use(bodyParser.json());
 app.use('/user',userRouter)
-
-
 
 app.listen('8080', function () {
     console.log('app is start at port 8080')
